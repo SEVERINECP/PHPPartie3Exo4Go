6278ef832f3e2d39350096390eab75eb60fca2fa
SEVERINECP <sevpicot@gmail.com>
2019-12-04 17:12:41 +0100

    PHPPartie3Exo4End

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$index = 'departments number';
+$valeur = 'departments name';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Partie3Exo4TableauAssociatif</title>
+  </head>
+  <body>
+    <h1>Departments</h1>
+    <?php
+    $index ['Nord'=>59, 'Pas-de-Calais'=>62, 'Somme'=>80, 'Oise'=>60, 'Aisne'=>02]
+     ?>
+  </body>
+</html>
